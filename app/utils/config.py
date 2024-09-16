f60d1d16ac49369993ea1f5b7aa3b2883ba15158
griggg <pvarst1230@gmail.com>
2024-09-16 19:53:00 +0300

    .

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,15 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from utils.secret import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_TEST_NAME
+from utils.secret import (
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME,
+    DB_TEST_NAME,
+    DB_TEST_USER,
+    DB_TEST_HOST,
+    DB_TEST_PASSWORD,
+)
 
-MODE = "TEST"
+MODE = "TEST "
 # MODE = "PROD"
 print(f"MODE DATABASE IS {MODE}")
 if MODE == "TEST":
+    # включать при разработке или запуске тестов
     url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_TEST_NAME}"
 else:
-    url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    url = f"postgresql://{DB_TEST_USER}:{DB_TEST_PASSWORD}@{DB_TEST_HOST}:5432/{DB_TEST_NAME}"
 
 
 # def make_engine():
--- app/utils/secret.py
+++ app/utils/secret.py
@@ -6,4 +6,8 @@ DB_USER=os.getenv("DB_USER")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
 DB_HOST=os.getenv("DB_HOST")
 DB_NAME=os.getenv("DB_NAME")
+
 DB_TEST_NAME=os.getenv("DB_TEST_NAME")
+DB_TEST_USER=os.getenv("DB_TEST_USER")
+DB_TEST_PASSWORD=os.getenv("DB_TEST_PASSWORD")
+DB_TEST_HOST=os.getenv("DB_TEST_HOST")
